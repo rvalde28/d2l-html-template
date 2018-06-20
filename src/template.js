c269d935dd8ea0948e370938e7b18fa6c95a6a02
ruben <ruben.valdez87@gmail.com>
2018-06-20 12:07:02 -0500

    SUT-9 Added script to check if in user mode -added checker to template.js, checks for parent of iframe

--- src/template.js
+++ src/template.js
@@ -7,4 +7,16 @@
     script.setAttribute('src', '/shared/js/smithu.js');
     window.top.document.body.appendChild(script);
   }
+
+    /**
+     * Checks if view is the user view.
+     */
+  if(parent.document.body.querySelector('iframe.d2l-iframe.d2l-iframe-fit-user-content')
+      .parentElement.classList.contains('d2l-fileviewer-text')){
+
+
+  }
+
 })();
+
+
