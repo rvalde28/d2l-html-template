8cd8ec9a2d4d3242aa91a7382680862a3e0c72bb
ruben <ruben.valdez87@gmail.com>
2018-06-26 17:52:34 -0500

    SUT-14B reduced code to move cells -removed swap function -added realign, addToColumns, splitRows, and getMaxRows function -realign: realigns entire cels -addToColumns adds cells to columns -splitRows splits array into rows to be put in to columns -getMaxRows returns max number of rows

--- src/template.js
+++ src/template.js
@@ -9,178 +9,112 @@
   }
 
     /**
-     * Checks if view is the user view, and swaps containers
+     * Function for swaping cells around
      */
-    var d2lIframe = parent.document.body.querySelector('iframe.d2l-iframe.d2l-iframe-fit-user-content');
-    if(d2lIframe){
-      if(d2lIframe.parentElement.classList.contains('d2l-fileviewer-text')){
-          document.body.classList.add('viewing');
-          swap();
+
+    let columns = $($('.grid-container')[0]).children();
+    let gridContiainer = $($('.grid-container')[1]);
+    let children = gridContiainer.children();
+
+    function getMaxRows(obj){
+        return Math.ceil(obj.length/3);
+    }
+
+    function splitRows(array, maxRows){
+      let columnArray = [[],[],[]];
+      let index = 0;
+
+      if(maxRows === 3){
+        if(array.length === 7){
+          console.log(array.length);
+          for (let i = 0; i < array.length; i++) {
+            console.log(index);
+
+            columnArray[index].push(array[i]);
+
+            if(index === 0){
+              if (i % 3 === 2) {
+                index++;
+              }
+            }
+            else{
+              if (i % 3 === 1) {
+                index++;
+              }
+            }
+          }
+        }
+        else {
+          for (let i = 0; i < array.length; i++) {
+            columnArray[index].push(array[i]);
+            if (i % 3 === 2) {
+              index++;
+            }
+          }
+        }
+      }
+      else if(maxRows === 2){
+        if(array.length === 4){
+          for (let i = 0; i < array.length; i++) {
+            columnArray[index].push(array[i]);
+            if(index === 0){
+              if (i % 2 === 1) {
+                index++;
+              }
+            }
+            else{
+
+
+              if (i % 2 === 0) {
+                index++;
+              }
+            }
+          }
+        }
+        else {
+          for (let i = 0; i < array.length; i++) {
+            columnArray[index].push(array[i]);
+            if (i % 2 === 1) {
+              index++;
+            }
+          }
+        }
+      }
+      else{
+        for(let i =0; i < array.length; i++){
+          columnArray[i].push(array[i]);
+        }
+      }
+
+      return columnArray;
+    }
+
+    function addToColumns(column, rowValues){
+      for(let i = 0; i < rowValues.length; i++){
+        $(column).append($(rowValues[i]))
+      }
+    }
+
+    function realign(){
+      let maxRows = getMaxRows(children);
+      let columnArray = splitRows(children,maxRows);
+
+      for(let i = 0; i < columns.length; i++){
+        console.log(columns[i]);
+        addToColumns(columns[i], columnArray[i]);
       }
     }
 
     /**
-     * Function for swaping cells around
+     * Checks if view is the user view, and swaps containers
      */
-
-    let colOne = $('#Column1');
-    let colTwo = $('#Column2');
-    let colThree = $('#Column3');
-    let gridContiainer = $($('.grid-container')[1]);
-    let children = gridContiainer.children();
-
-    function swap() {
-        if (children.length >= 8) {
-            let temp1 = children[1];
-            children[1] = children[3];
-            children[3] = temp1;
-
-            temp1 = children[2];
-            children[2] = children[6];
-            children[6] = temp1;
-
-            temp1 = children[5];
-            children[5] = children[7];
-            children[7] = temp1;
-
-            for (let i = 0; i < children.length; i++) {
-                children[i].remove();
-            }
-
-            for (let i = 0; i < children.length; i++) {
-                if(i % 3 === 0){
-                    colOne.append($(children[i]))
-                }
-                else if(i % 3 === 1){
-                    colTwo.append($(children[i]))
-                }
-                else if(i % 3 === 2){
-                    colThree.append($(children[i]))
-                }
-                else{}
-            }
-        }
-        else if (children.length === 7) {
-            let temp1 = children[1];
-            children[1] = children[3];
-            children[3] = temp1;
-
-            temp1 = children[2];
-            children[2] = children[6];
-            children[6] = temp1;
-
-            temp1 = children[5];
-            children[5] = children[2];
-            children[2] = temp1;
-
-            for (let i = 0; i < children.length; i++) {
-                children[i].remove();
-            }
-
-            for (let i = 0; i < children.length; i++) {
-                if(i % 3 === 0){
-                    colOne.append($(children[i]))
-                }
-                else if(i % 3 === 1){
-                    colTwo.append($(children[i]))
-                }
-                else if(i % 3 === 2){
-                    colThree.append($(children[i]))
-                }
-                else{}
-            }
-        }
-        else if (children.length === 6) {
-            let temp1 = children[1];
-            children[1] = children[3];
-            children[3] = temp1;
-
-            temp1 = children[2];
-            children[2] = children[4];
-            children[4] = temp1;
-
-            temp1 = children[1];
-            children[1] = children[4];
-            children[4] = temp1;
-
-
-            for (let i = 0; i < children.length; i++) {
-                children[i].remove();
-            }
-
-            for (let i = 0; i < children.length; i++) {
-                if(i % 3 === 0){
-                    colOne.append($(children[i]))
-                }
-                else if(i % 3 === 1){
-                    colTwo.append($(children[i]))
-                }
-                else if(i % 3 === 2){
-                    colThree.append($(children[i]))
-                }
-                else{}
-            }
-        }
-        else if (children.length === 5) {
-            let temp1 = children[1];
-            children[1] = children[3];
-            children[3] = temp1;
-
-            temp1 = children[1];
-            children[1] = children[2];
-            children[2] = temp1;
-
-            temp1 = children[2];
-            children[2] = children[4];
-            children[4] = temp1;
-
-            for (let i = 0; i < children.length; i++) {
-                children[i].remove();
-            }
-
-            for (let i = 0; i < children.length; i++) {
-                if(i % 3 === 0){
-                    colOne.append($(children[i]))
-                }
-                else if(i % 3 === 1){
-                    colTwo.append($(children[i]))
-                }
-                else if(i % 3 === 2){
-                    colThree.append($(children[i]))
-                }
-                else{}
-            }
-        }
-
-        else if (children.length === 4) {
-            let temp1 = children[1];
-            children[1] = children[3];
-            children[3] = temp1;
-
-            temp1 = children[1];
-            children[1] = children[2];
-            children[2] = temp1;
-
-            for (let i = 0; i < children.length; i++) {
-                children[i].remove();
-            }
-
-            for (let i = 0; i < children.length; i++) {
-                if(i % 3 === 0){
-                    colOne.append($(children[i]))
-                }
-                else if(i % 3 === 1){
-                    colTwo.append($(children[i]))
-                }
-                else if(i % 3 === 2){
-                    colThree.append($(children[i]))
-                }
-                else{}
-            }
-        }
+    let d2lIframe = parent.document.body.querySelector('iframe.d2l-iframe.d2l-iframe-fit-user-content');
+    if(d2lIframe){
+      if(d2lIframe.parentElement.classList.contains('d2l-fileviewer-text')){
+        document.body.classList.add('viewing');
+        realign()
+      }
     }
-
-
 })();
 
 
